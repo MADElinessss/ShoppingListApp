f3ebbbad13a88e7066f17a682fb245147207eb6c
MADElinessss <jyseen@naver.com>
2024-01-08 20:20:27 +0900

    여행 매거진 화면 만들기

---
+++ ShoppingListApp/MagazineTableViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  MagazineTableViewCell.swift
+//  ShoppingListApp
+//
+//  Created by 신정연 on 1/8/24.
+//
+
+import UIKit
+
+class MagazineTableViewCell: UITableViewCell {
+
+    @IBOutlet var cellImage: UIImageView!
+    @IBOutlet var title: UILabel!
+    @IBOutlet var subtitle: UILabel!
+    @IBOutlet var date: UILabel!
+}
