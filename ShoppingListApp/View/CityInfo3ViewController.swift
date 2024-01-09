7cb5c4b149e208d2f15260814d3105b76304e99f
MADElinessss <jyseen@naver.com>
2024-01-09 20:33:55 +0900

    3. ViewController + CollectionView + Segment

---
+++ ShoppingListApp/View/CityInfo3ViewController.swift
@@ -0,0 +1,57 @@
+//
+//  CityInfo3ViewController.swift
+//  ShoppingListApp
+//
+//  Created by 신정연 on 1/9/24.
+//
+
+import Kingfisher
+import UIKit
+
+class CityInfo3ViewController: UIViewController, UICollectionViewDelegate, UICollectionViewDataSource {
+    
+    let cities = CityInfo().city
+    
+    @IBOutlet var citiesCollectionView: UICollectionView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let xib = UINib(nibName: "CityInfo2CollectionViewCell", bundle: nil)
+        citiesCollectionView.register(xib, forCellWithReuseIdentifier: "CityInfo2CollectionViewCell")
+        
+        citiesCollectionView.dataSource = self
+        citiesCollectionView.delegate = self
+        
+        let layout = UICollectionViewFlowLayout()
+        
+        layout.minimumLineSpacing = 4
+        layout.minimumInteritemSpacing = 4
+        
+        let spacing : CGFloat = 8
+        let cellWidth : CGFloat = (UIScreen.main.bounds.width / 2.5)
+        
+        layout.itemSize = CGSize(width: cellWidth, height: cellWidth * 1.3)
+        layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing*3, bottom: spacing, right: spacing*3)
+        
+        citiesCollectionView.collectionViewLayout = layout
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return cities.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CityInfo2CollectionViewCell", for: indexPath) as! CityInfo2CollectionViewCell
+        
+        let url = URL(string: cities[indexPath.item].city_image)
+        cell.imageView.kf.setImage(with: url)
+        cell.imageView.layer.cornerRadius = 70
+        cell.titleLabel.text = "\(cities[indexPath.item].city_name) | \(cities[indexPath.item].city_english_name)"
+        cell.subtitleLabel.text = cities[indexPath.item].city_explain
+        
+        return cell
+    }
+    
+}
