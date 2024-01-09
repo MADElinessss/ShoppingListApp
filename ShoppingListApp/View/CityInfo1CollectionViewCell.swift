2d066beb5118af136adb6155039aaa51b3f9dc1d
MADElinessss <jyseen@naver.com>
2024-01-09 19:46:08 +0900

    1. CollectionViewController만 사용해서 UI 구현

---
+++ ShoppingListApp/View/CityInfo1CollectionViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  CityInfo1CollectionViewCell.swift
+//  ShoppingListApp
+//
+//  Created by 신정연 on 1/9/24.
+//
+
+import UIKit
+
+class CityInfo1CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet var titleLabel: UILabel!
+    @IBOutlet var subtitleLabel: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+}
