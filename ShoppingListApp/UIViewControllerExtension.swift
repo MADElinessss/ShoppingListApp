ca233692e60e010c5e303af11ab4e0e6d85dd875
MADElinessss <jyseen@naver.com>
2024-01-15 17:42:26 +0900

    10회차 과제 업데이트 -push 화면전환 - 웹뷰

---
+++ ShoppingListApp/UIViewControllerExtension.swift
@@ -0,0 +1,8 @@
+//
+//  UIViewControllerExtension.swift
+//  ShoppingListApp
+//
+//  Created by 신정연 on 1/15/24.
+//
+
+import Foundation
---
+++ ShoppingListApp/View/Magazine/WebViewController.swift
@@ -0,0 +1,29 @@
+//
+//  WebViewController.swift
+//  ShoppingListApp
+//
+//  Created by 신정연 on 1/15/24.
+//
+
+import UIKit
+
+class WebViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
