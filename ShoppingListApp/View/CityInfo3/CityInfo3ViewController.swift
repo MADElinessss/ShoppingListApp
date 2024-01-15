4e3e56d0bc718959ac3ced2bbf1be74b1b084713
MADElinessss <jyseen@naver.com>
2024-01-15 18:55:39 +0900

    ㅠㅠ

--- ShoppingListApp/View/CityInfo3/CityInfo3ViewController.swift
+++ ShoppingListApp/View/CityInfo3/CityInfo3ViewController.swift
@@ -14,6 +14,7 @@ class CityInfo3ViewController: UIViewController {
     
     @IBOutlet var citiesCollectionView: UICollectionView!
     @IBOutlet var categorySegment: UISegmentedControl!
+    @IBOutlet var searchBar: UISearchBar!
     
     override func viewDidLoad() {
         super.viewDidLoad()
