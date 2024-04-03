0730a8025f89faa8243d7ae2c72b2e412522dd99
MADElinessss <jyseen@naver.com>
2024-04-03 19:58:53 +0900

    [Feat] CityInfoView - ViewModel + Rx로 리팩토링

--- ShoppingListApp/View/CityInfo3/CityInfo3ViewController.swift
+++ ShoppingListApp/View/CityInfo3/CityInfo3ViewController.swift
@@ -6,20 +6,18 @@
 //
 
 import Kingfisher
+import RxCocoa
+import RxSwift
 import UIKit
 
 class CityInfo3ViewController: UIViewController {
 
-    // MARK: 관찰자 달기
-    var cities: [City] = CityInfo.city {
-        didSet {
-            citiesCollectionView.reloadData()
-        }
-    }
-    
     var originalList = CityInfo.city
     var domesticList = CityInfo.city.filter { $0.domestic_travel }
     var internationalList = CityInfo.city.filter { !$0.domestic_travel }
+    
+    let viewModel = CityInfoViewModel()
+    let disposeBag = DisposeBag()
     
     @IBOutlet var citiesCollectionView: UICollectionView!
     @IBOutlet var categorySegment: UISegmentedControl!
@@ -27,15 +25,52 @@ class CityInfo3ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
         configureSearchBar()
+        configureView()
+        bind()
+    }
+    
+    @IBAction func segmentValueChanged(_ sender: UISegmentedControl) {
+        // filterCities(segmentIndex: sender.selectedSegmentIndex)
+    }
+    
+    func bind() {
+        
+        searchBar.rx.text.orEmpty
+            .bind(to: viewModel.searchText)
+            .disposed(by: disposeBag)
+        
+        categorySegment.rx.selectedSegmentIndex
+            .bind(to: viewModel.segmentIndex)
+            .disposed(by: disposeBag)
+        
+        // 기존 CellForRowAt 기능 -> Rx
+        viewModel.cities
+            .bind(to: citiesCollectionView.rx.items(cellIdentifier: "CityInfo2CollectionViewCell", cellType: CityInfo2CollectionViewCell.self)) { (row, city, cell) in
+                let url = URL(string: city.city_image)
+                cell.imageView.kf.setImage(with: url)
+                cell.imageView.layer.cornerRadius = 70
+                cell.titleLabel.text = "\(city.city_name) | \(city.city_english_name)"
+                cell.subtitleLabel.text = city.city_explain
+            }
+            .disposed(by: disposeBag)
+        
+        // 기존 didselectItemAt -> Rx
+        citiesCollectionView.rx.modelSelected(City.self)
+            .subscribe { [weak self] city in
+                let storyboard = UIStoryboard(name: "TravelDetail", bundle: nil)
+                let viewController = storyboard.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
+                self?.navigationController?.pushViewController(viewController, animated: true)
+            }
+            .disposed(by: disposeBag)
+        
+        // 뷰가 뜨자마자 전체 도시 띄우기(근데 bind 내용 다 한 후에 호출해야 실행된다!!)
+        //viewModel.loadInitialCities()
+        // 근데 그냥 이럴거면 viewmodel의 cities를 behaviorRelay로 해서 초기값 설정해조라
+    }
+    
+    func configureView() {
 
-        let xib = UINib(nibName: "CityInfo2CollectionViewCell", bundle: nil)
-        citiesCollectionView.register(xib, forCellWithReuseIdentifier: "CityInfo2CollectionViewCell")
-        
-        citiesCollectionView.dataSource = self
-        citiesCollectionView.delegate = self
-        
         let layout = UICollectionViewFlowLayout()
         
         layout.minimumLineSpacing = 4
@@ -48,10 +83,10 @@ class CityInfo3ViewController: UIViewController {
         layout.sectionInset = UIEdgeInsets(top: spacing, left: spacing*3, bottom: spacing, right: spacing*3)
         
         citiesCollectionView.collectionViewLayout = layout
-    }
-    
-    @IBAction func segmentValueChanged(_ sender: UISegmentedControl) {
-        filterCities(segmentIndex: sender.selectedSegmentIndex)
+        
+        let nib = UINib(nibName: "CityInfo2CollectionViewCell", bundle: nil)
+        citiesCollectionView.register(nib, forCellWithReuseIdentifier: "CityInfo2CollectionViewCell")
+        
     }
 }
 
@@ -61,108 +96,6 @@ extension CityInfo3ViewController: UISearchBarDelegate {
     func configureSearchBar() {
         searchBar.delegate = self
         searchBar.showsCancelButton = true
-    }
-    
-    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
-
-        var filterData: [City] = []
-        var trimmedText = searchText.trimmingCharacters(in: .whitespacesAndNewlines)
-        var text = trimmedText.lowercased()
-        
-        let filteredList: [City]
-        switch categorySegment.selectedSegmentIndex {
-        case 1:
-            filteredList = domesticList
-        case 2:
-            filteredList = internationalList
-        default:
-            filteredList = originalList
-        }
-        
-        if text == "" {
-            cities = filteredList
-        } else {
-            for item in filteredList {
-                if item.city_english_name.lowercased().contains(text) || item.city_name.lowercased().contains(text) || item.city_explain.lowercased().contains(text) {
-                    filterData.append(item)
-                }
-            }
-            cities = filterData
-        }
-    }
-    
-    func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        var filterData: [City] = []
-        let text = searchBar.text?.lowercased() ?? ""
-        
-        let filteredList: [City]
-        switch categorySegment.selectedSegmentIndex {
-        case 1:
-            filteredList = domesticList
-        case 2:
-            filteredList = internationalList
-        default:
-            filteredList = originalList
-        }
-        
-        if text == "" {
-            cities = filteredList
-        } else {
-            for item in filteredList {
-                if item.city_english_name.lowercased().contains(text) || item.city_name.lowercased().contains(text) || item.city_explain.lowercased().contains(text) {
-                    filterData.append(item)
-                }
-            }
-            cities = filterData
-        }
-        view.endEditing(true)
-    }
-    
-    func searchBarCancelButtonClicked(_ searchBar: UISearchBar) {
-        searchBar.text = ""
-        cities = originalList
-        categorySegment.selectedSegmentIndex = 0
-        view.endEditing(true)
-    }
-}
-
-// MARK: CollectionView관련 Extension 분리
-extension CityInfo3ViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return cities.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CityInfo2CollectionViewCell", for: indexPath) as! CityInfo2CollectionViewCell
-        
-        let url = URL(string: cities[indexPath.item].city_image)
-        cell.imageView.kf.setImage(with: url)
-        cell.imageView.layer.cornerRadius = 70
-        cell.titleLabel.text = "\(cities[indexPath.item].city_name) | \(cities[indexPath.item].city_english_name)"
-        cell.subtitleLabel.text = cities[indexPath.item].city_explain
-        
-        return cell
-    }
-    
-    // MARK: 아이템 선택 시
-    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        let storyboard = UIStoryboard(name: "TravelDetail", bundle: nil)
-        let viewController = storyboard.instantiateViewController(withIdentifier: "DetailViewController") as! DetailViewController
-        navigationController?.pushViewController(viewController, animated: true)
-    }
-}
-
-// MARK: 도시 선택하는 로직
-extension CityInfo3ViewController {
-    func filterCities(segmentIndex index: Int) {
-        switch index {
-        case 1:
-            cities = CityInfo.city.filter { $0.domestic_travel }
-        case 2:
-            cities = CityInfo.city.filter { !$0.domestic_travel }
-        default:
-            cities = CityInfo.city
-        }
-        citiesCollectionView.reloadData()
+        searchBar.searchBarStyle = .prominent
     }
 }
--- ShoppingListApp/View/CityInfo3/CityInfoViewModel.swift
+++ ShoppingListApp/View/CityInfo3/CityInfoViewModel.swift
@@ -6,10 +6,72 @@
 //
 
 import Foundation
+import RxCocoa
+import RxSwift
 
 class CityInfoViewModel {
     
+    var cities = BehaviorRelay<[City]>(value: [])
+    let searchText = PublishRelay<String?>()// 초기값이 없어야 함->Publish
+    let segmentIndex = BehaviorRelay<Int>(value: 0)// 초기값이 있어야 함->Behavior
+    private var allCities = CityInfo.city
+    private let disposeBag = DisposeBag()
     
+    init() {
+        setUpBindings()
+    }
     
+    func setUpBindings() {
+
+        cities.accept(allCities) // 뷰 뜨자마자 모든 도시로 초기값 설정
+        
+        searchText
+            .withLatestFrom(segmentIndex) { searchText, segmentIndex in
+                return (searchText, segmentIndex)
+            }
+            .map(filterCities)
+            .bind(to: cities)
+            .disposed(by: disposeBag)
+        
+        segmentIndex
+            .withLatestFrom(searchText.startWith("")) { index, searchText in
+                return (searchText, index)
+            }
+            .map(filterCities)
+            .bind(to: cities)
+            .disposed(by: disposeBag)
+        
+    }
+    
+    // 초기값 설정
+//    func loadInitialCities() {
+//        let initialCities = filterCities(searchText: "", segmentIndex: segmentIndex.value)
+//        cities.accept(initialCities)
+//    }
+//    
+    private func filterCities(searchText: String?, segmentIndex: Int) -> [City] {
+        let filteredCities = filterBySegmentIndex(segmentIndex: segmentIndex)
+        
+        guard let searchText = searchText, !searchText.isEmpty else {
+            return filteredCities
+        }
+        
+        return filteredCities.filter {
+            $0.city_english_name.lowercased().contains(searchText.lowercased()) ||
+            $0.city_name.lowercased().contains(searchText.lowercased()) ||
+            $0.city_explain.lowercased().contains(searchText.lowercased())
+        }
+    }
+    
+    private func filterBySegmentIndex(segmentIndex: Int) -> [City] {
+        switch segmentIndex {
+        case 1:
+            return allCities.filter { $0.domestic_travel }
+        case 2:
+            return allCities.filter { !$0.domestic_travel }
+        default:
+            return allCities
+        }
+    }
     
 }
